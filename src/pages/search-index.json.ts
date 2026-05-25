6d384355123fc302e2e48d7f3202b787c25dd1da
digit-one-lab <digit.damir@icloud.com>
2026-05-25 03:59:28 +0200

    Point search results to articles route

---
+++ src/pages/search-index.json.ts
@@ -0,0 +1,12 @@
+import { getPosts, entrySlug } from '../lib/content';
+
+export async function GET() {
+  const posts = await getPosts();
+  return new Response(JSON.stringify(posts.map((post) => ({
+    title: post.data.title,
+    description: post.data.description,
+    tags: post.data.tags,
+    category: post.data.category,
+    url: `${import.meta.env.BASE_URL}articles/${entrySlug(post)}/`
+  }))), { headers: { 'Content-Type': 'application/json' } });
+}
